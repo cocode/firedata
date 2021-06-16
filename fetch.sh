d09879b84080e4fbb7af18abe4860819fac4432b
cocode <cocode@github.com>
2021-06-16 13:42:31 -0700

    Check in the newly downloaded json data, and the webpage created from that data.

--- fetch.sh
+++ fetch.sh
@@ -6,7 +6,14 @@ echo $PWD
 python3 --version
 ls -al
 ls -al data
+ls -al data/data_cal
 python3 get_cal_fire_data.py
-ls -al data
+ls -al data/data_cal
+git add data/data_cal
+git commit -m "Automatic add of new fire data"
+git add docs
+git commit -m "Add newly generated web page"
+git push
 python3 -m webpage.create_webpage
+cp webpage/fire_graphs.html docs/index.html
 echo "Done."
