4e40094eb8fa8a52161458e9b04f59154acb26ee
cocode <cocode@github.com>
2021-06-16 18:19:00 -0700

    Fix bug, where we were not pushing the website changes back. Also add comments.

--- fetch.sh
+++ fetch.sh
@@ -1,19 +1,23 @@
+set -euo pipefail
 PYTHONPATH=$PWD:$PWD/webpage
-echo $PYTHONPATH
-echo Add other actions to build,
-echo test, and deploy your project.
-echo $PWD
+echo PYTHONPATH: $PYTHONPATH
+echo PWD: $PWD
 python3 --version
-ls -al
-ls -al data
-ls -al data/data_cal
+
 python3 get_cal_fire_data.py
 ls -al data/data_cal
+ls -al data/data_us
+
+# Add and commit the new data
 git add data/data_cal
 git commit -m "Automatic add of new fire data"
-git add docs
-git commit -m "Add newly generated web page"
-git push
+
+# Build and commit the webpage.
 python3 -m webpage.create_webpage
 cp webpage/fire_graphs.html docs/index.html
+git add docs
+git commit -m "Add newly generated web page"
+
+# Push all changes
+git push
 echo "Done."
